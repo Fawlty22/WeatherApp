c631cabff6e4e8484e8011d071e85647cb2b97c9
Matthew Keys <Matthewjkeys22@gmail.com>
2021-10-21 13:25:25 -0600

    reload functions added and yes they work why would you even ask that stupid wuestion

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -144,6 +144,86 @@ var createPastLocation = function (data) {
     $('#past-searches').append(newPastLocationButton)
 }
 
+var reloadWeather = function (locationToLoad) {
+    
+    locationToLoad.replace(' ', '%20')
+    // format the url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationToLoad + "&appid=075971209128ea7c235a0302d27bd564&units=imperial";
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function (response) {
+            //request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log('success', data)
+                    showWeatherBox(data);
+                    reloadForecast(data);
+                    getCoords(data);
+                    createPastLocation(data);
+                });
+            } else {
+                console.log(response)
+                alert('Error: Please Enter A Valid City Name')
+            };
+        })
+        .catch(function (error) {
+            alert("Unable to connect to GitHub");
+        });
+
+
+};
+
+var reloadForecast = function (data) {
+    var now = moment()
+    //change display from none
+    $('#forecast-display').addClass('d-block')
+
+
+    for (i = 4; i < 40; i += 8) {
+        //variable for moment dates to handle weird iterating
+        k = ((i + 4) / 8) + 3
+
+        //make the card
+        var newCard = $('<div>')
+            .addClass('forecast-item bg-dark p-3');
+        //make the h3
+        var preDay = moment().day(k)
+        var newHeader = $('<h3>')
+            .text(moment(preDay).format('MM/DD/YYYY'))
+        //make img
+        var iconCode = data.list[i].weather[0].icon
+        var iconURL = 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png'
+        var newImage = $('<img>')
+            .attr('src', iconURL)
+        //make ul
+        var newList = $('<ul>')
+            .addClass('forecast-list fs-5 list-group')
+        //make li's
+        var newTemp = $('<li>')
+            .addClass('current-text p-2')
+            .text('Temp: ' + data.list[i].main.temp)
+        var newWind = $('<li>')
+            .addClass('current-text p-2')
+            .text('Wind: ' + data.list[i].wind.speed)
+        var newHumidity = $('<li>')
+            .addClass('current-text p-2')
+            .text('Humidity: ' + data.list[i].main.humidity)
+
+        //append header to div
+        newCard.append(newHeader)
+        //append img
+        newCard.append(newImage)
+        //append ul
+        newCard.append(newList)
+        //append li's to ul
+        newList.append(newTemp)
+        newList.append(newWind)
+        newList.append(newHumidity)
+
+        //append card to display
+        $('#forecast-container').append(newCard)
+    }
+}
 
 
 $('#input-area').on('click', 'button', function () {
@@ -156,5 +236,8 @@ $('#input-area').on('click', 'button', function () {
 
 $('#past-searches').on('click', 'button', function() {
     var locationToLoad = $(this).text()
-    console.log(locationToLoad)
+    // console.log(locationToLoad)
+    reloadWeather(locationToLoad);    
+    $('#forecast-container').empty();
+
 })
